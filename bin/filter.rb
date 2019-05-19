fefd284741ef685d5f0fefe0e5858a72d529ff5b
Andrew Cantino <cantino@users.noreply.github.com>
2019-05-18 18:34:19 -0700

    WIP

--- bin/filter.rb
+++ bin/filter.rb
@@ -19,7 +19,7 @@ def split_header(data)
   [header, body]
 end
 
-def process(data)
+def process(data, force_header: nil, format_for_frontpage: false)
   header, body = split_header(data)
 
   doc = Nokogiri::HTML::DocumentFragment.parse(body)
@@ -47,23 +47,51 @@ def process(data)
     }
   end
 
-  # Remove any footer, and old subscribe regions.
-  doc.css('#templateFooter, #subscribe-footer').each do |node|
-    node.remove
+  # Links should open in a new tab.
+  doc.css("a").each do |node|
+    node['target'] = "_blank"
   end
 
-  if header
-    '---' + header + '---' + doc.to_html
+  # Remove any footer and old subscribe regions.
+  doc.css('#templateFooter, #subscribe-footer').each(&:remove)
+
+  if format_for_frontpage
+    # Remove the email header text, if any.
+    doc.css('#templatePreheader').each(&:remove)
+
+    # Rename the title
+    doc.css('#templateHeader h1 span').each do |node|
+      node.inner_html = "Latest Issue"
+    end
+  end
+
+  if force_header || header
+    '---' + (force_header || header) + '---' + doc.to_html
   else
     doc.to_html
   end
 end
 
+# Update the archive
+
+greatest_issue_number, greatest_issue_file = -1, nil
 files = Dir['archive/_posts/*.html']
 files.each do |file|
-  puts "Processing #{file}"
+  issue_number = file[/Issue-(\d+)/, 1].to_i
+  puts "Processing #{file} (#{issue_number})"
   processed = process(File.read(file))
   File.open(file, 'w') do |file|
     file.print processed
   end
+
+  if issue_number > greatest_issue_number
+    greatest_issue_number = issue_number
+    greatest_issue_file = file
+  end
+end
+
+# Update the latest post on the frontpage
+existing_header, _ = split_header(File.read('index.md'))
+File.open('index.md', 'w') do |file|
+  file.print process(File.read(greatest_issue_file), force_header: existing_header, format_for_frontpage: true)
 end
